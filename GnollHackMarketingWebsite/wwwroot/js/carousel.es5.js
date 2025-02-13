9c5d888562fe6d089f3b28251e33a9c2ba07039c
Tommi Gustafsson <tommi.gustafsson@hyvanmielenpelit.fi>
2025-02-13 22:09:35 +0200

    Fixed carousel.

---
+++ GnollHackMarketingWebsite/wwwroot/js/carousel.es5.js
@@ -0,0 +1,24 @@
+﻿'use strict';
+
+$(function () {
+    var thumbnailCarousel = document.getElementById('carouselComponent');
+    var modalCarouselElement = document.getElementById('modalCarousel');
+    var modalCarousel = bootstrap.Carousel.getOrCreateInstance(modalCarouselElement);
+
+    thumbnailCarousel.addEventListener('slide.bs.carousel', function (event) {
+        if (event.direction === "right") {
+            modalCarousel.prev();
+        } else if (event.direction === "left") {
+            modalCarousel.next();
+        }
+    });
+
+    var modal = document.getElementById('fullscreenModal');
+    modal.addEventListener('show.bs.modal', function () {
+        $("section.orc-hunter").height("100vh");
+    });
+    modal.addEventListener('hide.bs.modal', function () {
+        $("section.orc-hunter").height("");
+    });
+});
+
---
+++ GnollHackMarketingWebsite/wwwroot/js/carousel.js
@@ -0,0 +1,23 @@
+﻿$(function () {
+    const thumbnailCarousel = document.getElementById('carouselComponent');
+    const modalCarouselElement = document.getElementById('modalCarousel');
+    var modalCarousel = bootstrap.Carousel.getOrCreateInstance(modalCarouselElement);
+
+    thumbnailCarousel.addEventListener('slide.bs.carousel', event => {
+        if (event.direction === "right") {
+            modalCarousel.prev();
+        }
+        else if (event.direction === "left") {
+            modalCarousel.next();
+        }
+    })
+
+    var modal = document.getElementById('fullscreenModal');
+    modal.addEventListener('show.bs.modal', () => {
+        $("section.orc-hunter").height("100vh");
+    });
+    modal.addEventListener('hide.bs.modal', () => {
+        $("section.orc-hunter").height("");
+    });
+});
+
