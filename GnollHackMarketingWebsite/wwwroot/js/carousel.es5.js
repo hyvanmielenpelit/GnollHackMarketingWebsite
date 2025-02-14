1f1e42b70a9a27a582dbca6907af37fb7ba381b0
Tommi Gustafsson <tommi.gustafsson@hyvanmielenpelit.fi>
2025-02-14 15:06:13 +0200

    Improved carousel further.

--- GnollHackMarketingWebsite/wwwroot/js/carousel.es5.js
+++ GnollHackMarketingWebsite/wwwroot/js/carousel.es5.js
@@ -1,18 +1,26 @@
 ﻿"use strict";
 
-var players = new Array(10);
+var players = new Array();
 
 function onYouTubeIframeAPIReady() {
-    $("div.modalPlayer").each(function (index) {
+    $("div.thumbnailPlayer, div.modalPlayer").each(function (index) {
         var videoID = $(this).attr("data-video-id");
         var id = $(this).attr("id");
 
-        players[index] = new YT.Player(id, {
+        players.push(new YT.Player(id, {
             videoId: videoID,
             playerVars: {
                 'playsinline': 1
             }
-        });
+        }));
+    });
+}
+
+function stopPlayers() {
+    players.forEach(function (player, index) {
+        if (player) {
+            player.stopVideo();
+        }
     });
 }
 
@@ -21,34 +29,43 @@ $(function () {
     var modalCarouselElement = document.getElementById('modalCarousel');
     var thumbnailCarousel = bootstrap.Carousel.getOrCreateInstance(thumbnailCarouselElement);
     var modalCarousel = bootstrap.Carousel.getOrCreateInstance(modalCarouselElement);
+    var autoMovingModal = false;
+    var autoMovingThumbnail = false;
 
     thumbnailCarouselElement.addEventListener('slide.bs.carousel', function (event) {
-        if (event.direction === "right") {
-            modalCarousel.prev();
-        } else if (event.direction === "left") {
-            modalCarousel.next();
+        if (!autoMovingThumbnail) {
+            autoMovingModal = true;
+            if (event.direction === "right") {
+                modalCarousel.prev();
+            } else if (event.direction === "left") {
+                modalCarousel.next();
+            }
+            autoMovingModal = false;
         }
+        stopPlayers();
     });
 
     modalCarouselElement.addEventListener('slide.bs.carousel', function (event) {
-        if (event.direction === "right") {
-            thumbnailCarousel.prev();
-        } else if (event.direction === "left") {
-            thumbnailCarousel.next();
+        if (!autoMovingModal) {
+            autoMovingThumbnail = true;
+            if (event.direction === "right") {
+                thumbnailCarousel.prev();
+            } else if (event.direction === "left") {
+                thumbnailCarousel.next();
+            }
+            autoMovingThumbnail = false;
         }
+        stopPlayers();
     });
 
     var modal = document.getElementById('fullscreenModal');
     modal.addEventListener('show.bs.modal', function () {
         $("section.orc-hunter").height("100vh");
+        stopPlayers();
     });
     modal.addEventListener('hide.bs.modal', function () {
         $("section.orc-hunter").height("");
-        players.forEach(function (player, index) {
-            if (player) {
-                player.stopVideo();
-            }
-        });
+        stopPlayers();
     });
 });
 
--- GnollHackMarketingWebsite/wwwroot/js/carousel.js
+++ GnollHackMarketingWebsite/wwwroot/js/carousel.js
@@ -1,56 +1,71 @@
-﻿var players = new Array(10);
+﻿var players = new Array();
 
 function onYouTubeIframeAPIReady() {
-    $("div.modalPlayer").each(function (index) {
+    $("div.thumbnailPlayer, div.modalPlayer").each(function (index) {
         var videoID = $(this).attr("data-video-id");
         var id = $(this).attr("id");
 
-        players[index] = new YT.Player(id, {
+        players.push(new YT.Player(id, {
             videoId: videoID,
             playerVars: {
                 'playsinline': 1
             }
-        });
+        }));
     })
 }
 
+function stopPlayers() {
+    players.forEach(function (player, index) {
+        if (player) {
+            player.stopVideo();
+        }
+    });
+}
 
 $(function () {
     const thumbnailCarouselElement = document.getElementById('carouselComponent');
     const modalCarouselElement = document.getElementById('modalCarousel');
     var thumbnailCarousel = bootstrap.Carousel.getOrCreateInstance(thumbnailCarouselElement);
     var modalCarousel = bootstrap.Carousel.getOrCreateInstance(modalCarouselElement);
+    var autoMovingModal = false;
+    var autoMovingThumbnail = false;
 
     thumbnailCarouselElement.addEventListener('slide.bs.carousel', event => {
-        if (event.direction === "right") {
-            modalCarousel.prev();
+        if (!autoMovingThumbnail) {
+            autoMovingModal = true;
+            if (event.direction === "right") {
+                modalCarousel.prev();
+            }
+            else if (event.direction === "left") {
+                modalCarousel.next();
+            }
+            autoMovingModal = false;
         }
-        else if (event.direction === "left") {
-            modalCarousel.next();
-        }
+        stopPlayers();
     })
 
     modalCarouselElement.addEventListener('slide.bs.carousel', event => {
-        if (event.direction === "right") {
-            thumbnailCarousel.prev();
+        if (!autoMovingModal) {
+            autoMovingThumbnail = true;
+            if (event.direction === "right") {
+                thumbnailCarousel.prev();
+            }
+            else if (event.direction === "left") {
+                thumbnailCarousel.next();
+            }
+            autoMovingThumbnail = false;
         }
-        else if (event.direction === "left") {
-            thumbnailCarousel.next();
-        }
+        stopPlayers();
     })
 
     var modal = document.getElementById('fullscreenModal');
     modal.addEventListener('show.bs.modal', () => {
         $("section.orc-hunter").height("100vh");
-
+        stopPlayers();
     });
     modal.addEventListener('hide.bs.modal', () => {
         $("section.orc-hunter").height("");
-        players.forEach(function (player, index) {
-            if (player) {
-                player.stopVideo();
-            }
-        });
+        stopPlayers();
     });
 });
 
