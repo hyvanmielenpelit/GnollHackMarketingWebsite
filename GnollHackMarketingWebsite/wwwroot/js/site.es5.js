916d6413dc122a8d9e578d88331041d8a14bde95
Tommi Gustafsson <tommi.gustafsson@hyvanmielenpelit.fi>
2025-06-04 17:20:34 +0300

    Added GitHub download badge and OS tooltips for the download badges.

--- GnollHackMarketingWebsite/wwwroot/js/site.es5.js
+++ GnollHackMarketingWebsite/wwwroot/js/site.es5.js
@@ -1,8 +1,14 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
 "use strict";
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) arr2[i] = arr[i]; return arr2; } else { return Array.from(arr); } }
+
+var tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+var tooltipList = [].concat(_toConsumableArray(tooltipTriggerList)).map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+});
 
 $(function () {
     $("#consentButton").on("click", function () {
--- GnollHackMarketingWebsite/wwwroot/js/site.js
+++ GnollHackMarketingWebsite/wwwroot/js/site.js
@@ -1,7 +1,9 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
 $(function () {
     $("#consentButton").on("click", function () {
         $("#consentDiv").hide();
